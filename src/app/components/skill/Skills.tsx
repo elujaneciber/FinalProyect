14d3cf8f4a9dd320ee96b7a92c041008b737df0d
elujaneciber <emanuel.lujan@udea.edu.co>
2024-06-07 21:10:00 -0500

    Update Skills

--- src/app/components/skill/Skills.tsx
+++ src/app/components/skill/Skills.tsx
@@ -1,4 +1,3 @@
-import { Punto } from "@components/utils";
 import { FirestoreSkillsRepository } from "@firebase/dao";
 import { Skill } from "./Skill";
 
@@ -6,6 +5,14 @@ export async function Skills() {
   const firestoreSkills = new FirestoreSkillsRepository();
   const skills = await firestoreSkills.get();
   const sortSkills = skills.sort((a, b) => a.order - b.order);
+  const chunkSize = 5;
+  const chunkedArray = Array.from(
+    { length: Math.ceil(sortSkills.length / chunkSize) },
+    (_, index) => {
+      const start = index * chunkSize;
+      return sortSkills.slice(start, start + chunkSize);
+    }
+  );
 
   return (
     <div className="bg-[#7c9a9262] rounded-tr-3xl rounded-bl-3xl p-8">
@@ -13,11 +20,16 @@ export async function Skills() {
         Habilidades
       </h3>
       <div className="flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-16 mb-2">
-        <div className="flex flex-col gap-2 text-gray-900 text-lg">
-          {sortSkills.map((content, index) => (
-            <Skill key={index} content={content} />
-          ))}
-        </div>
+        {chunkedArray.map((chunk, index) => (
+          <div
+            key={index}
+            className="flex flex-col gap-2 text-gray-900 text-lg"
+          >
+            {chunk.map((content, index) => (
+              <Skill key={index} content={content} />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
